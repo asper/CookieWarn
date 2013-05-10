8b3f83bb57f9a69c759ecca4997088fabcf58d9e
Pierre Aboucaya <p@asper.fr>
2013-05-10 03:36:40 +0300

    Update CookieWarn.js

--- CookieWarn.js
+++ CookieWarn.js
@@ -6,10 +6,13 @@ var CookieWarn = new Class(
         'class': null,
         container: null,
         auto: true,
-        html: '{message}: {ok} {more}'
+        html: '{message}: {ok} | {more}'
         message: 'This site uses cookies to enable persistent features between page changes and to gather anonymous statistics about our web site frequentation.',
         ok: 'Thanks for Telling Me',
+        okClass: 'cookieWarnOk',
         more: 'Why this message?',
+        moreClass: 'cookieWarnMore',
+        moreTarget: '_blank',
         link: 'http://cookie-warn.asper.fr'
     },
     initialize: function(options){
@@ -20,26 +23,37 @@ var CookieWarn = new Class(
         }
     },
     show: function(){
-        if(!this.accepted()){
-            var t = this,
-                o = t.options,
-                replacements = {
-                    message: o.message,
-                    ok: '<a href="#" class="cookieWarnOk">'+o.ok+'</a>',
-                    more: '<a href="'+o.link+'" class="cookieWarnMore" target="_blank">'+o.more+'</a>'
-                },
-                el = new Element('div', {
-                    id: o.id,
-                    class: o.class,
-                    html: o.html.substitute(replacements)
-                }),
-                okLink = el.getElement('.cookieWarnOk').addEvent('click', function(e){
-                    if(e){
-                        e.preventDefault();
-                    }
-                    t.accepted(true);
-                });
+        if(this.accepted()){
+            return true;
         }
+        var t = this,
+            o = t.options,
+            replacements = {
+                message: o.message,
+                ok: new Element('a', {
+                    href: '#',
+                    'class': o.okClass,
+                    html: o.ok
+                }),
+                more: new Element('a', {
+                    href: o.link,
+                    'class': o.moreClass,
+                    target: o.moreTarget,
+                    html: o.more
+                })
+            },
+            el = new Element('div', {
+                id: o.id,
+                'class': o.class,
+                html: o.html.substitute(replacements)
+            }),
+            okLink = el.getElement('.cookieWarnOk').addEvent('click', function(e){
+                if(e){
+                    e.preventDefault();
+                }
+                t.accepted(true);
+            });
+        el.inject(document.id(o.container));
     },
     accepted: function(value){
         return (value !== undefined) ? Cookie.write(this.options.cookieName, value) : Cookie.read(this.options.cookieName);
