9076706a475194787b3c01153ed0bdc20cb1c7f2
asper <p@asper.fr>
2013-05-11 18:31:04 -0400

    Modified

--- javascripts/CookieWarn.js
+++ javascripts/CookieWarn.js
@@ -10,44 +10,72 @@ var CookieWarn = new Class({
         'class': null,
         container: null,
         auto: true,
-        html: '{message}<br />{ok} | {more}',
-        message: 'This site uses cookies to enable persistent features between page changes and to gather anonymous statistics about our web site frequentation',
-        ok: 'Thanks for Telling Me',
-        okClass: 'cookieWarnOk',
+        
         more: 'Why this message?',
         moreClass: 'cookieWarnMore',
         moreTarget: '_blank',
-        link: 'http://cookie-warn.asper.fr'
+        moreLink: 'http://cookie-warn.asper.fr',
+        
+        warnMessage: 'This site uses cookies to enable persistent features between page changes and to gather anonymous statistics about our web site frequentation',
+        warnHtml: '{message}<br />{ok} | {more}',
+        warnOk: 'Thanks for Telling Me',
+        warnOkClass: 'cookieWarnOk',
+        
+        remindMessage: 'You have allowed cookies',
+        remindHtml: '{message}<br />{nok} | {more}',
+        remindNok: 'Do not allow cookies',
+        remindNokClass: 'cookieWarnNok'
     },
     initialize: function(options) {
         this.options.container = document.id(document.body);
         this.setOptions(options);
         if (this.options.auto) {
-            this.show();
+            var message = this.accepted() == true ? this.remind() : this.warn();
         }
     },
-    show: function() {
-        if (this.accepted()) {
-            return true;
-        }
+    warn: function() {
         var t = this,
             o = t.options,
             replacements = {
-                message: o.message,
-                ok: '<a href="#" class="'+o.okClass+'">'+o.ok+'</a>',
-                more: '<a href="'+o.link+'" class="'+o.moreClass+'" target="'+o.moreTarget+'">'+o.more+'</a>',
+                message: o.warnMessage,
+                ok: '<a href="#" class="'+o.warnOkClass+'">'+o.warnOk+'</a>',
+                more: '<a href="'+o.moreLink+'" class="'+o.moreClass+'" target="'+o.moreTarget+'">'+o.more+'</a>'
             },
             el = new Element('div', {
                 id: o.id,
                 'class': o.class,
-                html: o.html.substitute(replacements)
+                html: o.warnHtml.substitute(replacements)
             }),
-            okLink = el.getElement('.'+o.okClass);
+            okLink = el.getElement('.'+o.warnOkClass);
         okLink.addEvent('click', function(e) {
             if (e) {
                 e.preventDefault();
             }
-            t.accepted(true);
+            t.accepted(1);
+            window.location.reload();
+        });
+        el.inject(document.id(o.container));
+    },
+    remind: function() {
+        var t = this,
+            o = t.options,
+            replacements = {
+                message: o.remindMessage,
+                ok: '<a href="#" class="'+o.remindNokClass+'">'+o.remindNok+'</a>',
+                more: '<a href="'+o.moreLink+'" class="'+o.moreClass+'" target="'+o.moreTarget+'">'+o.more+'</a>'
+            },
+            el = new Element('div', {
+                id: o.id,
+                'class': o.class,
+                html: o.warnHtml.substitute(replacements)
+            }),
+            okLink = el.getElement('.'+o.remindNokClass);
+        okLink.addEvent('click', function(e) {
+            if (e) {
+                e.preventDefault();
+            }
+            t.accepted(0);
+            window.location.reload();
         });
         el.inject(document.id(o.container));
     },
